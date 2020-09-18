33b1a033b6e527870c051707b8606049f32f3194
David Gerding <dave@gerding.us>
2020-09-18 13:49:31 -0500

    Imported J. Montemagno's code from repo.
    -Runs on Android and Windows.

--- ContactsApp/ContactsApp/MainPage.xaml.cs
+++ ContactsApp/ContactsApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ContactsApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace ContactsApp
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new AddContactViewModel();
         }
     }
 }
